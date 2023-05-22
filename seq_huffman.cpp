5df90d74bdae26096880bbdd3dfc85e7a2d8fdc5
Luca <lucasantarella@hotmail.it>
2023-05-22 18:47:04 +0200

    reading from file

--- seq_huffman.cpp
+++ seq_huffman.cpp
@@ -9,6 +9,8 @@ HUFFMAN CODING (SEQUENTIAL):
 #include <iostream>
 #include <map>
 #include <fstream>
+#include "utimer.cpp"
+#include <chrono>
 using namespace std;
 
 #define SIZE 26
@@ -28,16 +30,32 @@ std::map<char, int> countFreq(string str)
     return mapStrOcc;
 }
 
-int main(int argc, char* argv[]){
-    string str = "this is an example of a huffman tree";
-    std::map<char, int> freq;
-    freq = countFreq(str);
+void printFreq(std::map<char, int> freq)
+{
     // Get an iterator pointing to the first element in the map
     std::map<char, int>::iterator it = freq.begin();
     while (it != freq.end())
     {
         std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
         ++it;
-  }
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    string strFile;
+    string str;
+    {utimer("reading file");
+        ifstream MyReadFile("txt_files/file_10M.txt");
+        while(getline (MyReadFile, str))
+        {
+            strFile += str;
+            strFile.push_back('\n');
+        }
+    }
+    std::map<char, int> freq;
+    freq = countFreq(strFile);
+    //printFreq(freq);
+
   return (0);
 }
