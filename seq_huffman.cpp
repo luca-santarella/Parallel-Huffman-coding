6575d9a1472c7ab681805f29700bb1f1f7572e5d
Luca <lucasantarella@hotmail.it>
2023-05-25 09:55:16 +0200

    seq code complete

--- seq_huffman.cpp
+++ seq_huffman.cpp
@@ -12,9 +12,11 @@ HUFFMAN CODING (SEQUENTIAL):
 #include <fstream>
 #include "utimer.hpp"
 #include <chrono>
+#include <bitset>
 using namespace std;
 
 #define MAX_TREE_HT 1000
+long n_chars;
 
 struct treeNode
 {
@@ -54,7 +56,7 @@ struct treeNode* newNode(char data, int freq)
     return myNewNode;
 }
 
-std::map<char, int> countFreq(string str)
+std::map<char, int> countFreq(std::string str)
 {
     // size of the string 'str'
     int n = str.size();
@@ -69,19 +71,31 @@ std::map<char, int> countFreq(string str)
     return mapStrOcc;
 }
 
-void printFreq(std::map<char, int> freq)
+
+void printFreq(std::map<char, int> map)
 {
     // Get an iterator pointing to the first element in the map
-    std::map<char, int>::iterator it = freq.begin();
-    while (it != freq.end())
+    std::map<char, int>::iterator it = map.begin();
+    while (it != map.end())
     {
         std::cout << "key: " << it->first << ", freq: " << it->second << std::endl;
         ++it;
     }
 }
 
+void printMap(std::map<char, std::string> map)
+{
+    // Get an iterator pointing to the first element in the map
+    std::map<char, std::string>::iterator it = map.begin();
+    while (it != map.end())
+    {
+        std::cout << "key: " << it->first << ", code: " << it->second << std::endl;
+        ++it;
+    }
+}
+
 template<typename Q>
-void initQueue(Q &prior_q, std::map<char, int> freq, tree* &binTree)
+void initQueue(Q &prior_q, std::map<char, int> freq, tree* &hufTree)
 {
     std::map<char, int>::iterator it = freq.begin();
     while (it != freq.end())
@@ -89,7 +103,7 @@ void initQueue(Q &prior_q, std::map<char, int> freq, tree* &binTree)
         struct treeNode *myNewNode;
         myNewNode = newNode(it->first, it->second);
         prior_q.push(myNewNode);
-        binTree->size++;
+        hufTree->size++;
         ++it;
     }
 }
@@ -98,51 +112,61 @@ template<typename Q>
 void printQueue(std::string_view name, Q q)
 {
     for (std::cout << name << ": \n"; !q.empty(); q.pop())
-        std::cout << "key: " << q.top()->data << " freq: " << q.top()->freq << endl ;
+        std::cout << "key: " << q.top()->data << " freq: " << q.top()->freq << std::endl ;
     std::cout << '\n';
 }
 
 // function to check if this node is leaf
-int isLeaf(struct treeNode* node){
+int isLeaf(struct treeNode* node)
+{
 
     return !(node->left) && !(node->right);
 }
 
 //print an array of size n
-void printArr(int arr[], int n){
-    int i;
-    for (i = 0; i < n; ++i)
-        cout << arr[i];
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        std::cout << arr[i];
 
-    cout << "\n";
+    std::cout << "\n";
 }
 
-void printCodes(struct treeNode* root, int arr[],
-                int top){
+void setCode(char data, int arr[], int n, std::map<char, std::string> &codes)
+{
+    std::string code;
+    for (int i=0; i < n; i++)
+    {
+        code += to_string(arr[i]);
+    }
+    codes[data] = code;
+}
 
+
+void traverseTree(struct treeNode* root, int arr[], int top, std::map<char, std::string> &codes)
+{
     //assign 0 to left edge and recur
     if (root->left) {
 
         arr[top] = 0;
-        printCodes(root->left, arr, top + 1);
+        traverseTree(root->left, arr, top + 1, codes);
     }
 
     //assign 1 to right edge and recur
     if (root->right) {
 
         arr[top] = 1;
-        printCodes(root->right, arr, top + 1);
+        traverseTree(root->right, arr, top + 1, codes);
     }
 
     if (isLeaf(root)) {
 
-        cout << root->data << ": ";
-        printArr(arr, top);
+        setCode(root->data, arr, top, codes);
     }
 }
 
 template<typename Q>
-void buildHufTree(Q &prior_q, tree* &binTree)
+void buildHufTree(Q &prior_q, tree* &hufTree)
 {
     while(prior_q.size() != 1)
     {
@@ -171,57 +195,126 @@ void buildHufTree(Q &prior_q, tree* &binTree)
         prior_q.push(internalNode);
 
         //increase size of binary tree because of new internal node
-        binTree->size++;
+        hufTree->size++;
     }
+}
+
+std::string HuffmanCoding(std::string stringToCode, std::map<char, std::string> codes)
+{
+    std::string codedStr;
+
+    int n = stringToCode.size();
+    for(int i=0; i <n; i++)
+    {
+        char charToCode = stringToCode[i];
+        codedStr += codes[charToCode];
+    }
+    return codedStr;
+}
+
+std::string padCodedStr(std::string str)
+{
+    int size = str.size();
+    int bits = size % 8;
+    bits = 8 - bits;
+
+    //pad the string
+    str.append(bits, '0');
+    return str;
+}
+
+char convertToASCII(std::string binaryString)
+{
+    int decimalValue = 0;
+    for (char bit : binaryString) 
+        decimalValue = (decimalValue << 1) + (bit - '0');
+
+    return static_cast<char>(decimalValue);
+}
+
+std::string encodeStrASCII(std::string binaryString)
+{
+    std::string encodedStr;
+    for(int i=0; i<binaryString.size(); i+=8)
+        encodedStr += convertToASCII(binaryString.substr(i, 8));
+
+    return encodedStr;
 }
 
 int main(int argc, char* argv[])
 {
     //***READING FROM TXT FILE***
-    string strFile;
-    string str;
+    std::string strFile;
+    std::string str;
     long usecs;
+    std::string inputFile = "example";
     {utimer t0("reading file", &usecs);
-        ifstream MyReadFile("txt_files/example.txt");
-        while(getline (MyReadFile, str))
+        
+        ifstream inFile("txt_files/"+inputFile+".txt");
+        while(getline (inFile, str))
         {
             strFile += str;
             //strFile.push_back('\n');
         }
     }
-    cout << "reading in " << usecs << endl;
-    //cout << strFile << endl;
+    std::cout << "reading in " << usecs << " usecs" << std::endl;
 
     //***COUNTING FREQUENCIES***
     std::map<char, int> freq;
     freq = countFreq(strFile);
-    printFreq(freq);
+    //printFreq(freq);
+    n_chars = freq.size();
 
     //***INITIALIZE PRIORITY QUEUE AND BINARY TREE***
     // Max priority to lowest freq node
     std::priority_queue<treeNode*, vector<treeNode*>, node_comparison> prior_q; 
 
     //representation of the binary tree
-    struct tree *binTree = (struct tree*) malloc (sizeof(struct tree));
-    binTree->size = 0;
+    struct tree *hufTree = (struct tree*) malloc (sizeof(struct tree));
+    hufTree->size = 0;
 
-    initQueue(prior_q, freq, binTree);
-
-
-    //printQueue("priority_queue", prior_q);
+    //initialize the priority queue
+    initQueue(prior_q, freq, hufTree);
 
     //*** BUILD HUFFMAN TREE
-    //compute until there are just two nodes to be processed
-    buildHufTree(prior_q, binTree);
+    //build the huffman tree using the priority queue
+    buildHufTree(prior_q, hufTree);
 
     //set root 
     struct treeNode* myRoot = prior_q.top();
-    binTree->root = myRoot;
+    hufTree->root = myRoot;
 
-    //print Huffman codes using
+    //array used to get Huffman codes
     int arr[MAX_TREE_HT], top = 0;
-    printCodes(myRoot, arr, top);
-    
-    //printQueue("priority_queue", prior_q);
+
+    //map <char, huffman code>
+    std::map<char, std::string> codes;
+
+    //*GET HUFFMAN CODES USING HUFFMAN TREE
+    //traverse the Huffman tree and set codes
+    traverseTree(myRoot, arr, top, codes);
+    std::cout << std::endl;
+    //printMap(codes);
+
+    std::string codedStr = HuffmanCoding(strFile, codes);
+
+    //pad the coded string to get a multiple of 8
+    if(codedStr.size() % 8 != 0)
+        codedStr = padCodedStr(codedStr);
+
+    //encode binary string (result of Huffman coding) as ASCII characters 
+    codedStr = encodeStrASCII(codedStr);
+    std::ofstream outFile("out_files/coded_"+inputFile+".bin", std::ios::binary);
+
+    if (outFile.is_open()) 
+    {
+        outFile.write(codedStr.c_str(), codedStr.size());
+        outFile.close();  // Close the file
+    }
+    else
+    {
+        std::cout << "Unable to open the file." << std::endl;
+    }
+
   return (0);
 }
